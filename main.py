ec5ecd215cb3396ea7d5d8a584c66dbc05488bce
shibamoy <119690630+shibamoy@users.noreply.github.com>
2025-01-21 14:09:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import turtle
-from turtle import Screen
+from turtle import Screen, Turtle
 import pandas
 
 pd = pandas.read_csv("50_states.csv")
 state = (pd["state"])
 x_point = pd["x"]
 y_point = pd["y"]
-print(pd[pd.state == "Ohio"])
-
-
+print(pd[pd.state == "Ohio"]["x"])
+state_dict = {state: (x_val, y_val) for state, x_val, y_val in zip(state, x_point, y_point)}
+total = 50
+current_states = 50 - len(state_dict)
 
 
 screen = Screen()
@@ -17,6 +18,24 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-user_answer = screen.textinput(title= "Guess the states", prompt="What is the next state?").title()
-print(user_answer)
+def print_state(coor, state_name):
+    writer = Turtle()
+    writer.penup()
+    writer.goto(coor)
+    writer.pendown()
+    writer.write(state_name)
+
+
+while current_states != 50:
+    total = 50
+    current_states = 50 - len(state_dict)
+    user_answer = screen.textinput(title= f"Guess the states{current_states}/{total}", prompt="What is the next state?").title()
+    if user_answer in state_dict:
+        state = user_answer
+        coor = state_dict[user_answer]
+        print_state(coor,state)
+        state_dict.pop(user_answer)
+
+
 turtle.mainloop()
+
